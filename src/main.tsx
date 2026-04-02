bc070313e83c5a7a8582fe21131cf72cc5c55528
Hind Bitit <bitithind@gmail.com>
2026-04-02 01:38:31 +0100

    TP4

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './features/auth/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
